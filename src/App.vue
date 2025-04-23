42e4aca059100e7585fcdba9a162508157854c3f
xihan123 <srxqzxs@vip.qq.com>
2025-04-23 18:33:58 +0800

    refactor(App.vue): 移除未使用的导入

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import {onBeforeUnmount, onMounted} from 'vue';
+import {onMounted} from 'vue';
 import AppHeader from '@/components/common/AppHeader.vue';
 import AppFooter from '@/components/common/AppFooter.vue';
 import {useThemeStore} from '@/store/themeStore';
